f17ef1b6b992efae015dced16244a005fdb9f61e
Elif Duymaz Yılmaz <97849933+eduymaz@users.noreply.github.com>
2025-05-15 17:59:14 +0300

    rfm v2

--- crm-analysis/rfm/rfm.py
+++ crm-analysis/rfm/rfm.py
@@ -1,42 +1,10 @@
 ###############################################################
-# RFM ile Müşteri Segmentasyonu (Customer Segmentation with RFM)
+# 1. Business Problem
 ###############################################################
-
-# 1. İş Problemi (Business Problem)
-# 2. Veriyi Anlama (Data Understanding)
-# 3. Veri Hazırlama (Data Preparation)
-# 4. RFM Metriklerinin Hesaplanması (Calculating RFM Metrics)
-# 5. RFM Skorlarının Hesaplanması (Calculating RFM Scores)
-# 6. RFM Segmentlerinin Oluşturulması ve Analiz Edilmesi (Creating & Analysing RFM Segments)
-# 7. Tüm Sürecin Fonksiyonlaştırılması
-
-###############################################################
-# 1. İş Problemi (Business Problem)
-###############################################################
-
-# Bir e-ticaret şirketi müşterilerini segmentlere ayırıp bu segmentlere göre
-# pazarlama stratejileri belirlemek istiyor.
-
-# Veri Seti Hikayesi
-# https://archive.ics.uci.edu/ml/datasets/Online+Retail+II
-
-# Online Retail II isimli veri seti İngiltere merkezli online bir satış mağazasının
-# 01/12/2009 - 09/12/2011 tarihleri arasındaki satışlarını içeriyor.
-
-# Değişkenler
-#
-# InvoiceNo: Fatura numarası. Her işleme yani faturaya ait eşsiz numara. C ile başlıyorsa iptal edilen işlem.
-# StockCode: Ürün kodu. Her bir ürün için eşsiz numara.
-# Description: Ürün ismi
-# Quantity: Ürün adedi. Faturalardaki ürünlerden kaçar tane satıldığını ifade etmektedir.
-# InvoiceDate: Fatura tarihi ve zamanı.
-# UnitPrice: Ürün fiyatı (Sterlin cinsinden)
-# CustomerID: Eşsiz müşteri numarası
-# Country: Ülke ismi. Müşterinin yaşadığı ülke.
 
 
 ###############################################################
-# 2. Veriyi Anlama (Data Understanding)
+# 2. Data Understanding
 ###############################################################
 
 import datetime as dt
@@ -51,7 +19,7 @@ df.head()
 df.shape
 df.isnull().sum()
 
-# essiz urun sayisi nedir?
+# for unique
 df["Description"].nunique()
 
 df["Description"].value_counts().head()
@@ -67,7 +35,7 @@ df["TotalPrice"] = df["Quantity"] * df["Price"]
 df.groupby("Invoice").agg({"TotalPrice": "sum"}).head()
 
 ###############################################################
-# 3. Veri Hazırlama (Data Preparation)
+# 3. Data Preparation
 ###############################################################
 
 df.shape
@@ -78,7 +46,7 @@ df.dropna(inplace=True)
 df = df[~df["Invoice"].str.contains("C", na=False)]
 
 ###############################################################
-# 4. RFM Metriklerinin Hesaplanması (Calculating RFM Metrics)
+# 4. Calculating RFM Metrics
 ###############################################################
 
 # Recency, Frequency, Monetary
@@ -103,12 +71,10 @@ rfm.shape
 
 
 ###############################################################
-# 5. RFM Skorlarının Hesaplanması (Calculating RFM Scores)
+# 5. Calculating RFM Scores
 ###############################################################
 
 rfm["recency_score"] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
-
-# 0-100, 0-20, 20-40, 40-60, 60-80, 80-100
 
 rfm["frequency_score"] = pd.qcut(rfm['frequency'].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
 
@@ -124,11 +90,11 @@ rfm[rfm["RFM_SCORE"] == "55"]
 rfm[rfm["RFM_SCORE"] == "11"]
 
 ###############################################################
-# 6. RFM Segmentlerinin Oluşturulması ve Analiz Edilmesi (Creating & Analysing RFM Segments)
+# 6. Creating & Analysing RFM Segments
 ###############################################################
 # regex
 
-# RFM isimlendirmesi
+# RFM named
 seg_map = {
     r'[1-2][1-2]': 'hibernating',
     r'[1-2][3-4]': 'at_Risk',
@@ -157,69 +123,4 @@ new_df["new_customer_id"] = new_df["new_customer_id"].astype(int)
 new_df.to_csv("new_customers.csv")
 
 rfm.to_csv("rfm.csv")
-
-###############################################################
-# 7. Tüm Sürecin Fonksiyonlaştırılması
-###############################################################
-
-def create_rfm(dataframe, csv=False):
-
-    # VERIYI HAZIRLAMA
-    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
-    dataframe.dropna(inplace=True)
-    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
-
-    # RFM METRIKLERININ HESAPLANMASI
-    today_date = dt.datetime(2011, 12, 11)
-    rfm = dataframe.groupby('Customer ID').agg({'InvoiceDate': lambda date: (today_date - date.max()).days,
-                                                'Invoice': lambda num: num.nunique(),
-                                                "TotalPrice": lambda price: price.sum()})
-    rfm.columns = ['recency', 'frequency', "monetary"]
-    rfm = rfm[(rfm['monetary'] > 0)]
-
-    # RFM SKORLARININ HESAPLANMASI
-    rfm["recency_score"] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
-    rfm["frequency_score"] = pd.qcut(rfm["frequency"].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
-    rfm["monetary_score"] = pd.qcut(rfm['monetary'], 5, labels=[1, 2, 3, 4, 5])
-
-    # cltv_df skorları kategorik değere dönüştürülüp df'e eklendi
-    rfm["RFM_SCORE"] = (rfm['recency_score'].astype(str) +
-                        rfm['frequency_score'].astype(str))
-
-
-    # SEGMENTLERIN ISIMLENDIRILMESI
-    seg_map = {
-        r'[1-2][1-2]': 'hibernating',
-        r'[1-2][3-4]': 'at_risk',
-        r'[1-2]5': 'cant_loose',
-        r'3[1-2]': 'about_to_sleep',
-        r'33': 'need_attention',
-        r'[3-4][4-5]': 'loyal_customers',
-        r'41': 'promising',
-        r'51': 'new_customers',
-        r'[4-5][2-3]': 'potential_loyalists',
-        r'5[4-5]': 'champions'
-    }
-
-    rfm['segment'] = rfm['RFM_SCORE'].replace(seg_map, regex=True)
-    rfm = rfm[["recency", "frequency", "monetary", "segment"]]
-    rfm.index = rfm.index.astype(int)
-
-    if csv:
-        rfm.to_csv("rfm.csv")
-
-    return rfm
-
-df = df_.copy()
-
-rfm_new = create_rfm(df, csv=True)
-
-
-
-
-
-
-
-
-
 
